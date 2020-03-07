31120b0b66a4d13709113475e1a26ef72626859a
Hasitha Samarasekara <hasitha.dks@gmail.com>
2020-03-07 13:31:40 +0530

    Initial Commit

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class Login extends Component{
+    render(){
+        return <div>
+            <h2>My Application</h2>
+        </div>
+    }
+}
