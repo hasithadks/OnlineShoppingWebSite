19bb3d1808f8390965199c86b5ca4d80163fa9af
DhanushaDevinda <devindarajapaksha12@gmail.com>
2020-04-24 12:58:16 +0530

    add-item-component,edit-item-component,item-list-component created

---
+++ src/components/add-item-component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class AddItem extends Component{
+    render(){
+        return(
+            <div>
+                <h3>--Welcome to Add Item Component--</h3>
+            </div>
+        )
+    }
+}
---
+++ src/components/item-list-component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ItemList extends Component{
+    render(){
+        return(
+            <div>
+                <h3>--Welcome to List Item Component--</h3>
+            </div>
+        )
+    }
+}
