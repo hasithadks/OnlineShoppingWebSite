6f372e9deef5f38f0ace369ca00b9b46462598d8
Hasitha Samarasekara <hasitha.dks@gmail.com>
2020-05-14 15:08:45 +0530

    Add Cart to server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const staffRouter = require('./routes/managementstaff');
 const favouriteRouter = require('./routes/favouriteProduct');
 const deliveryDetailsRouter = require('./routes/deliveryDetails');
 const productCategoryRouter = require('./routes/productcategory');
+const cartRouter = require('./routes/cart');
 
 app.use('/products', productRouter);
 app.use('/quantity', quantityRouter);
@@ -32,6 +33,7 @@ app.use('/mstaff', staffRouter);
 app.use('/favouriteProduct', favouriteRouter);
 app.use('/deliveryDetails', deliveryDetailsRouter);
 app.use('/pcategory',productCategoryRouter);
+app.use('/cart',cartRouter);
 
 const userRouter = require('./routes/user');
 app.use('/users',userRouter);
